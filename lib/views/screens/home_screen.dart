12000aecc696024c81016be902712155b464c0ea
mohammadmansour814 <mohammadadmobonunity@gmail.com>
2024-09-29 17:59:21 +0300

    added feature last day, last week, or last month

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -9,12 +9,32 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String _selectedTimeRange = 'Last Day'; // Default to 'Last Day'
+
   @override
   void initState() {
     super.initState();
-    // Fetch trending repositories for a specific date range (e.g., last month)
+    _fetchDataForSelectedTimeRange();
+  }
+
+  // Helper function to fetch data based on the selected time range
+  void _fetchDataForSelectedTimeRange() {
+    String dateRange;
+    DateTime now = DateTime.now();
+
+    if (_selectedTimeRange == 'Last Day') {
+      dateRange =
+          now.subtract(Duration(days: 1)).toIso8601String().split('T')[0];
+    } else if (_selectedTimeRange == 'Last Week') {
+      dateRange =
+          now.subtract(Duration(days: 7)).toIso8601String().split('T')[0];
+    } else {
+      dateRange =
+          now.subtract(Duration(days: 30)).toIso8601String().split('T')[0];
+    }
+
     Provider.of<RepositoryViewModel>(context, listen: false)
-        .fetchRepositories('2024-08-29');
+        .fetchRepositories(dateRange);
   }
 
   @override
@@ -22,7 +42,25 @@ class _HomeScreenState extends State<HomeScreen> {
     final viewModel = Provider.of<RepositoryViewModel>(context);
 
     return Scaffold(
-      appBar: AppBar(title: Text('Trending Repositories')),
+      appBar: AppBar(
+        title: Text('Trending Repositories'),
+        actions: [
+          DropdownButton<String>(
+            value: _selectedTimeRange,
+            items: [
+              DropdownMenuItem(value: 'Last Day', child: Text('Last Day')),
+              DropdownMenuItem(value: 'Last Week', child: Text('Last Week')),
+              DropdownMenuItem(value: 'Last Month', child: Text('Last Month')),
+            ],
+            onChanged: (value) {
+              setState(() {
+                _selectedTimeRange = value!;
+                _fetchDataForSelectedTimeRange(); // Fetch data when time range changes
+              });
+            },
+          ),
+        ],
+      ),
       body: viewModel.repositories.isEmpty
           ? Center(child: CircularProgressIndicator()) // Show loading indicator
           : ListView.builder(
